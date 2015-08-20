cd3d7fbd04f331191c77099c5cb1262ee315419a
Benny Michielsen <bennym@me.com>
2015-08-20 07:41:02 +0200

    Updated to capture when the sensor changes state
    We now capture one pulse (two changes of the sensor) and write it via serial resolves #6 and resolves #4

--- Arduino/watermeter-pulse-indicator/watermeter-pulse-indicator.ino
+++ Arduino/watermeter-pulse-indicator/watermeter-pulse-indicator.ino
@@ -1,19 +1,30 @@
 const int sensorPin = 2;     
 const int ledPin =  3;      
 
-int sensorState = 0;         
+int sensorState = 0; 
+int previousState = 0;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(sensorPin, INPUT);
+  Serial.begin(9600);  
+  Serial.println(millis());
+  previousState = digitalRead(sensorPin);
 }
 
 void loop() {
   sensorState = digitalRead(sensorPin);
-  if (sensorState == HIGH) {
-    digitalWrite(ledPin, HIGH);
-  }
-  else {
-    digitalWrite(ledPin, LOW);
+ 
+  if (sensorState != previousState) {
+    if(previousState == HIGH)
+    {
+      digitalWrite(ledPin, HIGH);    
+      Serial.println(millis());
+      delay(1000);
+       Serial.println(millis());
+          digitalWrite(ledPin, LOW);  
+    }  
+    previousState = sensorState;
+
   }
 }
