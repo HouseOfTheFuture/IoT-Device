0c216abf252284ec6f8666b199810a354da4a68f
HansP <hspeeters@gmail.com>
2015-10-05 21:33:59 +0200

    Working listener

--- HouseOfTheFuture.IoTHub/HouseOfTheFuture.IotHub.Tester/Program.cs
+++ HouseOfTheFuture.IoTHub/HouseOfTheFuture.IotHub.Tester/Program.cs
@@ -14,15 +14,27 @@ namespace HouseOfTheFuture.IotHub.Tester
 
         static void Main(string[] args)
         {
+            GroupPort = 5321;
+
             var udp = new UdpClient();
-            var groupEP = new IPEndPoint(IPAddress.Broadcast, GroupPort);
+            var groupEP = new IPEndPoint(IPAddress.Any, GroupPort);
 
             var str4 = "Smart";
 
-            var sendBytes4 = Encoding.ASCII.GetBytes(str4);
+            //var sendBytes4 = Encoding.ASCII.GetBytes(str4);
 
-            udp.Send(sendBytes4, sendBytes4.Length, groupEP);
-            byte[] receiveBytes = udp.Receive(ref groupEP);
+            //udp.Send(sendBytes4, sendBytes4.Length, groupEP);
+            udp.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udp.ExclusiveAddressUse = false;
+            udp.Client.Bind(groupEP);
+            udp.BeginReceive(new AsyncCallback(re), null);
+
+            Console.Read();
+        }
+
+        private static void re(IAsyncResult ar)
+        {
+            throw new NotImplementedException();
         }
     }
 }
