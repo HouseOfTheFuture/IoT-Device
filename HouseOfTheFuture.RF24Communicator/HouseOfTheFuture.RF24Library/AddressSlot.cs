0453b5dd52970d016b64fc657a04c66d85b59038
Dieter Sneyders <dieter.sneyders@infosupport.com>
2015-10-24 14:52:17 +0200

    start of RF24 Communicator for Raspberry Pi - Not working yet

---
+++ HouseOfTheFuture.RF24Communicator/HouseOfTheFuture.RF24Library/AddressSlot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseOfTheFuture.RF24Library
+{
+    public enum AddressSlot
+        {
+            Zero = Registers.RX_ADDR_P0,
+            One = Registers.RX_ADDR_P1,
+            Two = Registers.RX_ADDR_P2,
+            Three = Registers.RX_ADDR_P3,
+            Four = Registers.RX_ADDR_P4,
+            Five = Registers.RX_ADDR_P5,
+        }
+}
---
+++ HouseOfTheFuture.RF24Communicator/HouseOfTheFuture.RF24Library/NRFDataRate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseOfTheFuture.RF24Library
+{
+    public enum NRFDataRate
+    {
+        DR1Mbps,
+        DR2Mbps,
+        DR250kbps,
+    }
+}
